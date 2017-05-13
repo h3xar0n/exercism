06fc5ead579398ac96412cb8d52aa325fd01c8ad
h3xar0n <aron@glambyadelina.com>
2017-05-13 05:40:45 -0400

    Passes first two tests, needs to recognize Z

---
+++ javascript/pangram/pangram.js
@@ -0,0 +1,19 @@
+var Pangram = function(string) {
+  this.value = string
+}
+
+Pangram.prototype.isPangram = function() {
+  var string = this.value
+  string = string.toLowerCase().replace(/[^a-z]/g,'')
+  
+  var letters = "abcdefghijklmnopqrstuvwxyz"
+
+  for (var i = 0; i < 26; i++)
+    if (string.indexOf(letters[i]) < 0) {
+      return false
+    } else {
+      return true
+    }
+}
+
+module.exports = Pangram;

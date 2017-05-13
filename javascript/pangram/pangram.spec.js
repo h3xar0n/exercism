35aa3fc12c8e56a64b947cacb6b1c0d5bcbb20f8
h3xar0n <aron@glambyadelina.com>
2017-05-13 16:07:36 -0400

    Passes first 4 tests

--- javascript/pangram/pangram.spec.js
+++ javascript/pangram/pangram.spec.js
@@ -12,12 +12,12 @@ describe('Pangram()', function()  {
     expect(pangram.isPangram()).toBe(true);
   });
 
-  xit("missing character 'x'", function()  {
+  it("missing character 'x'", function()  {
     var pangram = new Pangram("a quick movement of the enemy will jeopardize five gunboats");
     expect(pangram.isPangram()).toBe(false);
   });
 
-  xit("another missing character 'x'", function() {
+  it("another missing character 'x'", function() {
     var pangram = new Pangram("the quick brown fish jumps over the lazy dog");
     expect(pangram.isPangram()).toBe(false);
   });

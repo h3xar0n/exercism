8ec034ac2a7c649c3b9f3208ec471e7e84fd84b8
h3xar0n <aron@glambyadelina.com>
2017-06-09 18:51:40 -0700

    Gigasecond not working exactly

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,13 +6,12 @@ Bob.prototype.hey = function(string) {
   var punctuation = string.slice(-1)
 
   var typeCase = string.toUpperCase();
-
   
   if ((typeCase == string) && string.match(/[a-z]/i)) {  // Shouting
     return 'Whoa, chill out!';
   } else if (punctuation == '?') {                       // Question
     return 'Sure.';
-  } else if (string.replace(/\s+/g, '') == '') {
+  } else if (string.replace(/\s+/g, '') == '') {         // Blank space of any size
     return 'Fine. Be that way!';
   } else {                                               // Default
     return 'Whatever.';
